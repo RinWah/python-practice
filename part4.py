293ce75c5fc9c4f9fa305bd6100fb1b04c8e2ddc
Rin <rinnuo041@gmail.com>
2025-06-19 15:23:48 -0400

    worked on part 4

--- part4.py
+++ part4.py
@@ -1,26 +1,13 @@
-a_number = 5
-another_number = 5.3
-my_name = "rin"
-many_numbers = [2, 5, 7, 23, 1, 4, 10]
-more_numbers = range(10)
-
-# check data type using the type() function
-print(type(a_number))
-print(type(another_number))
-print(type(my_name))
-print(type(many_numbers))
-print(type(more_numbers))
-
-# prints out #'s from 0-9
-for item in range(10):
-    print(item)
-
-for item in more_numbers: # same as for item in range(10):
-    print(item)
-
-# won't work since it's not iterable -- aka can't iterate through its elements like a list, same with another_number
-# for item in a_number: # a_number is an int
-#     print(item)
-
-for item in my_name: # iterable
-    print(item)
+many_numbers = [2, 5, 6, 23, 1, 4, 10]
+print(many_numbers)
+many_numbers.append(5000)
+print(many_numbers)
+many_numbers.remove(23)
+print(many_numbers)
+many_numbers.pop(0)
+print(many_numbers)
+removed = many_numbers.pop(0)
+print(f"removed {removed}")
+many_numbers.append(4)
+print(many_numbers)
+print(many_numbers.count(4))
